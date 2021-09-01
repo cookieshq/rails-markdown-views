206fc52c4a121f09a0e6685a3d3de7fd06e687c1
Romaric Pascal <hello@romaricpascal.is>
2021-09-01 13:51:45 +0100

    Properly initialize template handler to be reloadable

--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -1,1 +1,3 @@
-ActionView::Template.register_template_handler :md, MarkdownHandler
+Rails.application.reloader.to_prepare do
+  ActionView::Template.register_template_handler :md, MarkdownHandler
+end
